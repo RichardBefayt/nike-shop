eb3ce91e99d2c89be131341c907415078c426050
Richard Befayt <you@example.comsprichie.rb@gmail.com>
2023-02-27 18:16:33 +0100

    produits phares terminés, début avis

--- script.js
+++ script.js
@@ -24,9 +24,29 @@ function prev() {
 menu.addEventListener("click", () => {
     menu.classList.toggle("fa-times");
     navbar.classList.toggle("active");
-})
+});
 
 window.addEventListener("scroll", () => {
     menu.classList.remove("fa-times");
     navbar.classList.remove("active");
-})
+});
+
+// Agrandissement de la petite image à la place de la grande (produits phares)
+document.querySelectorAll(".featured-image-1").forEach(image_1 => {
+    image_1.addEventListener("click", () => {
+        const src = image_1.getAttribute("src");
+        document.querySelector(".big-image-1").src = src;
+    });
+});
+document.querySelectorAll(".featured-image-2").forEach(image_2 => {
+    image_2.addEventListener("click", () => {
+        const src = image_2.getAttribute("src");
+        document.querySelector(".big-image-2").src = src;
+    });
+});
+document.querySelectorAll(".featured-image-3").forEach(image_3 => {
+    image_3.addEventListener("click", () => {
+        const src = image_3.getAttribute("src");
+        document.querySelector(".big-image-3").src = src;
+    });
+});
