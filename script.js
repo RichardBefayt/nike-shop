c9e66e9d989fdd1c5050f6ec1927336fc1f592ec
Richard Befayt <you@example.comsprichie.rb@gmail.com>
2023-02-27 14:51:58 +0100

    home terminé, début services

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+// Déclarations des variables
 const menu = document.querySelector("#menu-bar");
 const navbar = document.querySelector(".navbar");
 
+const slides = document.querySelectorAll(".slide-container");
+let index = 0;
+
+// Fonctions
+function next() {
+    slides[index].classList.remove("active");
+    index = (index + 1) % slides.length;
+
+    slides[index].classList.add("active");
+}
+
+function prev() {
+    slides[index].classList.remove("active");
+    index = (index - 1 + slides.length) % slides.length;
+
+    slides[index].classList.add("active");
+}
+
+// Évènements
 menu.addEventListener("click", () => {
     menu.classList.toggle("fa-times");
     navbar.classList.toggle("active");
